1d5360b30a25d40e50adc014798077ffbb52d180
Samuel Maiko <sammymaiko721@gmail.com>
2024-11-07 16:59:45 +0300

    changed baseUrl to the hosted backend

--- src/axios/instance.js
+++ src/axios/instance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getCookie } from "../Cookies/Cookie";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://budgettyapp.pythonanywhere.com/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
